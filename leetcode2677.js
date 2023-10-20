d5dc15423cd382e65058e37b1a5c40ff8d1204d2
Abdullah Aziz <abdullahazees4@gmail.com>
2023-10-21 01:05:40 +0500

    files added

---
+++ leetcode2677.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function (arr, size) {
+  let chunkedarr = [];
+  for (let i = 0; i < arr.length; i += size) {
+    chunkedarr.push(arr.slice(i, size + i));
+  }
+  return chunkedarr;
+};
